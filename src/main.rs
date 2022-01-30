5d809deb73f6815583b685daa9f20cf88f51ddd9
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2022-01-29 22:20:28 -0500

    :tada: First draft of dynamic document creation and insert

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 #[macro_use]
 extern crate log;
 extern crate log4rs;
+extern crate byte_unit;
 
 use std::error::Error;
+use byte_unit::n_mb_bytes;
+use chrono::prelude::*;
 use clap::Parser;
-use mongodb::bson::{doc, Document};
-use mongodb::{Client, options::ClientOptions};
+use lipsum::lipsum;
+use mongodb::bson::{doc, Document, Uuid};
+use mongodb::{Client};
+use crate::FieldTypes::{TypeDate, TypeInt, TypeText};
+use rand::{Rng};
+use serde::{Deserialize, Serialize};
 
 /// Load generator for MongoDB Atlas built using Rust
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Opt {
+pub struct Opt {
     /// MongoDB connection string
     #[clap(short, long)]
     conn: Option<String>,
@@ -21,7 +28,7 @@ struct Opt {
 
     /// Number of top level fields in the document. Default 10
     #[clap(short, long, parse(try_from_str), default_value_t = 10)]
-    num_fields: usize,
+    num_fields: u16,
 
     /// Nesting depth. Default 0
     #[clap(long, parse(try_from_str), default_value_t = 0)]
@@ -73,17 +80,106 @@ async fn main()  -> Result<(), Box<dyn Error + Send + Sync>> {
     let opt = Opt::parse();
     println!("{:#?}", opt);
 
-    mongodb_load_gen().await
+    mongodb_load_gen(opt).await
 }
 
-pub async fn mongodb_load_gen() -> Result<(), Box<dyn Error + Send + Sync>> {
+pub async fn mongodb_load_gen(opt: Opt) -> Result<(), Box<dyn Error + Send + Sync>> {
     let client = Client::with_uri_str("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false").await?;
     let database = client.database("rmdb");
     let collection = database.collection("load");
-    let document = doc! {
-        "name": "rrajesh1979",
-        "dob": 1979
-    };
-    collection.insert_one(document, None).await?;
+    collection.insert_one(create_doc(opt), None).await?;
     Ok(())
+}
+
+fn create_doc(opt: Opt) -> Document {
+    let mut mongo_doc = MongoDoc {
+        document: Document::new(),
+        num_fields: opt.num_fields,
+        depth: 0,
+        txt_len: opt.text_size,
+        binary: opt.binary,
+    };
+    mongo_doc.add_id();
+    mongo_doc.add_fields();
+    mongo_doc.add_binary();
+    mongo_doc.document
+}
+
+fn create_string(len: usize) -> String {
+    let random_text = lipsum(len);
+    random_text.to_string()
+}
+
+#[derive(Serialize, Deserialize)]
+struct MongoDoc {
+    document: Document,
+    num_fields: u16,
+    depth: usize,
+    txt_len: usize,
+    binary: Option<bool>,
+}
+
+trait Create {
+    fn add_fields(&mut self);
+    fn add_id(&mut self);
+    fn add_binary(&mut self);
+    fn field_type(&self, field_num: u16) -> FieldTypes;
+}
+
+impl Create for MongoDoc {
+    fn add_fields(&mut self) {
+        let mut field_num: u16 = 0;
+        while field_num < self.num_fields {
+            let f_type = self.field_type(field_num);
+            let i: u32 = rand::thread_rng().gen();
+            let t: DateTime<Utc> = Utc::now();
+
+            let s = create_string(self.txt_len);
+            match f_type {
+                TypeInt => self.document.insert(format!("{}{}", "fld", field_num), i),
+                TypeDate => self.document.insert(format!("{}{}", "fld", field_num), chrono::Utc::now() ),
+                _ => self.document.insert(format!("{}{}", "fld", field_num), s),
+            };
+            field_num += 1;
+        }
+    }
+
+    fn add_id(&mut self) {
+        self.document.insert("_id", Uuid::new());
+    }
+
+    fn add_binary(&mut self) {
+        match Some(self.binary) {
+            None => {
+                info!("Inside None");
+            }
+            Some(_) => {
+                info!("Inside Some");
+                let result = n_mb_bytes(2);
+                self.document.insert(format!("{}{}", "fld", "_binary"), "result");
+            }
+        };
+    }
+
+    fn field_type(&self, field_num: u16) -> FieldTypes {
+        if field_num == 0 {
+            TypeInt
+        } else if field_num == 1 {
+            TypeDate
+        } else if field_num == 3 {
+            TypeText
+        } else if field_num % 3 == 0 {
+            TypeInt
+        } else if field_num % 5 == 0 {
+            TypeDate
+        } else {
+            TypeText
+        }
+    }
+}
+
+enum FieldTypes {
+    TypeInt,
+    TypeDate,
+    TypeText,
 }
